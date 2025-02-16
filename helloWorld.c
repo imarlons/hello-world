2a5b039ed5660228a5bc91fbcdc404f9020f841f
imarlons <marlon.silva@aluno.fmpsc.edu.br>
2025-02-16 14:31:37 -0300

    ajustando o java e estruturando o hello world em C

---
+++ helloWorld.c
@@ -0,0 +1,6 @@
+// hello world em C
+#include <stdio.h>
+int main() {
+    printf("hello world");
+    return 0;
+}
--- helloWorld.java
+++ helloWorld.java
@@ -1,5 +1,5 @@
 // hello world em java
-public class Main {
+public class helloWorld {
     public static void main(String[] args) {
         System.out.println("hello world!");
     }
